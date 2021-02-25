4b5ad06a89cc96dc7d2eb37f661c496259705dc6
patrick.roth <patrick.roth@me.com>
2021-02-25 21:37:18 +0100

    frontEnd

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+    <h1>About my blog</h1>
+    <p>
+        Lyrisch: Hinter den Wortbergen
+        Jemand musste Josef K. verleumdet haben, denn ohne 
+        dass er etwas Böses getan hätte, wurde er eines Morgens 
+        verhaftet. »Wie ein Hund!« sagte er, es war, 
+        als sollte die Scham ihn überleben.
+    </p>
+    <p>
+        Weit hinten, hinter den Wortbergen, 
+        fern der Länder Vokalien und Konsonantien leben 
+        die Blindtexte. Abgeschieden wohnen Sie in Buchstabhausen 
+        an der Küste des Semantik, eines großen Sprachozeans. 
+        Ein kleines Bächlein namens Duden fließt durch ihren 
+        Ort und versorgt sie mit den nötigen Regelialien.
+        </p>
+    <p>
+        Lyrisch: Hinter den Wortbergen
+        Jemand musste Josef K. verleumdet haben, denn ohne 
+        dass er etwas Böses getan hätte, wurde er eines Morgens 
+        verhaftet. »Wie ein Hund!« sagte er, es war, 
+        als sollte die Scham ihn überleben.
+    </p>
+    <p>
+        Weit hinten, hinter den Wortbergen, 
+        fern der Länder Vokalien und Konsonantien leben 
+        die Blindtexte. Abgeschieden wohnen Sie in Buchstabhausen 
+        an der Küste des Semantik, eines großen Sprachozeans. 
+        Ein kleines Bächlein namens Duden fließt durch ihren 
+        Ort und versorgt sie mit den nötigen Regelialien.
+        </p>
+    </>
+);
+
+export default AboutPage;
